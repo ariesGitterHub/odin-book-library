500bf6b0f56ceddb9be3c4215ca21d81d6b40911
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2024-05-07 18:31:34 -0400

    Adding book cover assets to facilitate toggle function

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+
+const toggleBookCover = document.querySelectorAll(".toggle-book-cover");
