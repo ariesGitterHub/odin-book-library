5d22f1169499fed0603c839b0832b76432abdb4b
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2024-05-10 17:01:12 -0400

    Wiring up cover/info views for books via various buttons

--- javascript.js
+++ javascript.js
@@ -1,7 +1,28 @@
+// const myLibrary = [
+//     {
+//         title: "The Hobbit",
+//         author: "J.R.R. Tolkien",
+//         type: "Fiction",
+//         genre: "Young Adult",
+//         pages: 256,
+//         read: "Yes"
+//     }
+// ];
+
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+console.log(myLibrary);
+
+class Book {
+  constructor(title, author, type, genre, pages, imgsrc, read) {
+    this.title = title;
+    this.author = author;
+    this.type = type;
+    this.genre = genre;
+    this.pages = pages;
+    this.imgsrc = imgsrc;
+    this.read = read
+  }
 }
 
 function addBookToLibrary() {
@@ -9,4 +30,32 @@ function addBookToLibrary() {
 }
 
 
-const toggleBookCover = document.querySelectorAll(".toggle-book-cover");
+btnInfo = document.querySelector("#btn-info");
+btnXout = document.querySelector("#btn-xout");
+
+bookCardCover = document.querySelector("book-card-cover");
+bookCardNavBot = document.querySelector("book-card-nav-bot");
+
+bookCardInfo = document.querySelector("book-card-info");
+bookCardNavTop = document.querySelector("book-card-nav-top");
+
+btnInfo.addEventListener("click", showInfo);
+btnXout.addEventListener("click", hideInfo);
+
+function showInfo() {
+    bookCardCover.style.display = "none";
+    bookCardNavBot.style.display = "none";
+
+    bookCardInfo.style.display = "flex";
+    bookCardNavTop.style.display = "flex";
+}
+
+function hideInfo() {
+    bookCardCover.style.display = "flex";
+    bookCardNavBot.style.display = "flex";
+
+    bookCardInfo.style.display = "none";
+    bookCardNavTop.style.display = "none";
+}
+
+
